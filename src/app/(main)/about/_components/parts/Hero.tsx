bd93d532bab224d70f811ca2fd79309e4fb17bab
Muhammad Fadhil Kholaf <fadhilgd123@gmail.com>
2025-08-11 10:49:42 +0700

    feat: update about page

--- src/app/(main)/about/_components/parts/Hero.tsx
+++ src/app/(main)/about/_components/parts/Hero.tsx
@@ -1,39 +1,26 @@
-import Image from "next/image";
-
-import FadhilKholaf from "@/public/images/home/fadhilkholaf.jpg";
-
 const Hero = () => {
     return (
         <section className="flex h-fit w-full flex-col gap-16">
             <div>
                 <h1>About Me</h1>
             </div>
-            <main className="flex h-fit w-full flex-col-reverse gap-16 sm:flex-row sm:justify-between">
-                <div className="flex flex-col gap-4 sm:w-1/2">
-                    <div>
-                        <h3>Muhammad Fadhil Kholaf</h3>
-                    </div>
-                    <p>
-                        I am a software engineering student at SMK Telkom
-                        Malang(Graduated). I was born in 2006 in Tulungagung,
-                        and I spent the first 16 years of my life in Tulungagung
-                        and Trenggalek. My journey to becoming a programmer
-                        started when I was about 16 years old.
-                    </p>
-                    <p>
-                        In my first year of high school, I learned Java and
-                        object-oriented programming (OOP), and I thought it was
-                        fun, to be honest. In the second year, I chose Node.js
-                        and React.js as my areas of expertise and started
-                        learning web development.
-                    </p>
+            <main className="flex flex-col gap-4">
+                <div>
+                    <h3>Muhammad Fadhil Kholaf</h3>
                 </div>
-                <Image
-                    src={FadhilKholaf}
-                    alt="Fadhil Kholaf"
-                    priority
-                    className="aspect-square h-fit w-2/3 max-w-[300px] self-center rounded-lg object-cover sm:self-baseline"
-                />
+                <p>
+                    I’m a software engineering graduate from SMK Telkom Malang,
+                    born in 2006 in Tulungagung. I spent most of my early life
+                    between Tulungagung and Trenggalek. My journey into
+                    programming began at around 16 years old.
+                </p>
+                <p>
+                    In my first year of high school, I explored Java and
+                    object-oriented programming (OOP) — and quickly discovered
+                    how much I enjoyed coding. By my second year, I shifted my
+                    focus to web development, specializing in Node.js and
+                    React.js.
+                </p>
             </main>
         </section>
     );
